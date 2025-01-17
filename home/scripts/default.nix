18d9ce495ba50b6dea0f62f278fd4039cfa2faba
loner <loner.games@gmail.com>
2025-01-17 15:08:33 +0800

    add ascii script

--- home/scripts/default.nix
+++ home/scripts/default.nix
@@ -2,10 +2,11 @@
   let
     maxfetch = pkgs.writeScriptBin "maxfetch" (builtins.readFile ./maxfetch.sh);
     shutdown = pkgs.writeScriptBin "shutdown" (builtins.readFile ./shutdown.sh);
+    ascii = pkgs.writeScriptBin "ascii" (builtins.readFile ./ascii.sh);
 in {
   home.packages = with pkgs; [
     maxfetch
     shutdown
-    fuzzel
+    ascii
   ];
 }
