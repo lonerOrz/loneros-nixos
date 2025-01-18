e84252c0f8e4338982317f94d5154c6f6cf1af53
loner <loner.games@gmail.com>
2025-01-18 16:31:25 +0800

    update version

--- home/scripts/default.nix
+++ home/scripts/default.nix
@@ -1,7 +1,7 @@
 {pkgs, ...}:
   let
     maxfetch = pkgs.writeScriptBin "maxfetch" (builtins.readFile ./maxfetch.sh);
-    shutdown = pkgs.writeScriptBin "shutdown" (builtins.readFile ./shutdown.sh);
+    shutdown = pkgs.writeScriptBin "shut" (builtins.readFile ./shutdown.sh);
     ascii = pkgs.writeScriptBin "ascii" (builtins.readFile ./ascii.sh);
 in {
   home.packages = with pkgs; [
