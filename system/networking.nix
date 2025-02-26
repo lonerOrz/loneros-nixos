df217ec89440d62b70a2db8b7a07a5be2403ebc9
loner <loner.games@gmail.com>
2025-02-27 06:27:57 +0800

    cancel resolved

--- system/networking.nix
+++ system/networking.nix
@@ -10,21 +10,21 @@
 
   # networking
   networking.networkmanager.enable = true;
-  networking.networkmanager.dns = "systemd-resolved";
+  networking.networkmanager.dns = "default"; # one of "default", "dnsmasq", "systemd-resolved", "none"
   networking.hostName = "${host}";
   networking.timeServers = options.networking.timeServers.default ++ [ "pool.ntp.org" ];
   networking.enableIPv6 = true;
 
   # DNS 解析服务
-  services.resolved = {
-    enable = true;
-    dnssec = "false";
-    dnsovertls = "opportunistic";
-    fallbackDns = [
-      "8.8.4.4"
-      "114.114.114.114"
-    ];
-  };
+  # services.resolved = {
+  #   enable = true;
+  #   dnssec = "false";
+  #   dnsovertls = "opportunistic";
+  #   fallbackDns = [
+  #     "8.8.4.4"
+  #     "114.114.114.114"
+  #   ];
+  # };
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
