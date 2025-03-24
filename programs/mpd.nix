b0efa111ca457b077e1613541cfafb80032404f6
loner <loner.games@gmail.com>
2025-03-24 10:02:12 +0800

    fixed mpd service

--- programs/mpd.nix
+++ programs/mpd.nix
@@ -8,19 +8,22 @@
   # 启用 MPD 服务
   services.mpd = {
     enable = true;
+    startWhenNeeded = true;
     user = "${username}"; # MPD 运行的用户
     #group = "${username}";  # MPD 运行的用户组
+    network = {
+      listenAddress = "127.0.0.1";
+      port = 6606;
+    };
     musicDirectory = "/home/${username}/Music/localmusic";
+    playlistDirectory = "/home/${username}/.config/mpd/playlists";
+    dbFile = "/home/${username}/.config/mpd/database";
 
     # 指定 mpd.conf 配置文件路径
     extraConfig = ''
-      playlist_directory      "/home/${username}/.config/mpd/playlists"
-      db_file                 "/home/${username}/.config/mpd/database"
       log_file                "/home/${username}/.config/mpd/log"
       pid_file                "/home/${username}/.config/mpd/mpd.pid"
       state_file              "/home/${username}/.config/mpd/state"
-      bind_to_address         "localhost"
-      port                    "6600"
       log_level               "default"
       restore_paused          "yes"
       auto_update             "yes"
