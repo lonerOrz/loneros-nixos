1d331a776968b3b955d9ccace22f3b8c27cd707f
loner <loner.games@gmail.com>
2025-03-29 16:02:16 +0800

    setting docker rootless

--- programs/docker.nix
+++ programs/docker.nix
@@ -6,11 +6,29 @@
 }:
 {
   virtualisation.docker.enable = true;
+  virtualisation.docker.enableOnBoot = true;
   # 如果使用 btrfs 文件系统，可以设置存储驱动
   virtualisation.docker.storageDriver = "btrfs";
   virtualisation.docker.rootless = {
     enable = true; # 启用无根模式
     setSocketVariable = true; # 设置 DOCKER_HOST 变量
+    daemon.settings = {
+      # data-root = "/home/${username}/docker-data"; # 设置 Docker 数据存储路径
+      registry-mirrors = [
+        "https://registry.cn-hangzhou.aliyuncs.com"
+        "https://docker-0.unsee.tech"
+      ];
+      #userland-proxy = false;          # 禁用 userland-proxy
+      experimental = true; # 启用实验性功能
+      dns = [
+        "8.8.8.8"
+        "8.8.4.4"
+        "114.114.114.114"
+      ]; # 设置 Docker 使用的 DNS
+      #metrics-addr = "0.0.0.0:9323";   # 启用度量指标
+      #ipv6 = true;                    # 启用 IPv6
+      #fixed-cidr-v6 = "fd00::/80";    # 设置固定 IPv6 地址范围
+    };
   };
   # 配置 Docker Daemon 的其他设置
   virtualisation.docker.daemon.settings = {
