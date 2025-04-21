1b2a489ac0fcce5a47984d6360bd815e3f3fd75d
loner <2788892716@qq.com>
2025-04-22 03:23:54 +0800

    add comment

--- system/pipewire.nix
+++ system/pipewire.nix
@@ -6,7 +6,7 @@
       alsa.enable = true;
       alsa.support32Bit = true;
       jack.enable = true;
-      pulse.enable = true;
+      pulse.enable = true; # PulseAudio 兼容层,产生.pulse-cookie文件
       wireplumber = {
         enable = true;
         extraConfig = {
