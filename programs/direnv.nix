aef36698d10a1c2a93609741a2f81ee2bfbe8336
loner <2788892716@qq.com>
2025-05-05 12:23:39 +0800

    disable direnv silent

--- programs/direnv.nix
+++ programs/direnv.nix
@@ -6,8 +6,11 @@
   # 启用 direnv 并确保支持 nix-direnv
   programs.direnv = {
     enable = true;
+    loadInNixShell = true; # nix develop 中启用 direnv 的加载
     enableFishIntegration = true; # 启用 direnv 在 fish 中的集成
+    # enableBashIntegration = true;
+    # enableBashIntegration = true;
     nix-direnv.enable = true; # 启用 nix-direnv 以与 nix 环境配合使用
-    silent = true; # 启用direnv日志记录的隐藏
+    silent = false; # 启用direnv日志记录的隐藏
   };
 }
