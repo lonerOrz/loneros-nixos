e47c2d34141f998ab1e129ed2eb93d6b942533b9
loner <2788892716@qq.com>
2025-05-21 19:26:27 +0800

    add granix cache

--- system/nix.nix
+++ system/nix.nix
@@ -19,8 +19,14 @@
         "nix-command"
         "flakes"
       ];
-      substituters = [ "https://hyprland.cachix.org" ];
-      trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
+      substituters = [ 
+        "https://hyprland.cachix.org"
+        "https://cache.garnix.io" # add garnix cache form github loneros-nixos repo
+      ];
+      trusted-public-keys = [ 
+        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+        "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+      ];
       trusted-users = [
         "root"
         "${username}"
