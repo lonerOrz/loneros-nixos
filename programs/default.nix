ee631a2da978f8977187db4d54a44133a7453eba
loner <2788892716@qq.com>
2025-05-25 23:41:25 +0800

    add jellyfin server

--- programs/default.nix
+++ programs/default.nix
@@ -1,32 +1,36 @@
 {
   imports = [
+    # program
     ./nh.nix
-    ./docker.nix
     ./fcitx5.nix
-    ./mpd.nix
-    ./rclone.nix
     ./spicetify.nix
     # ./virtualbox.nix
     ./virt-manager.nix # kvm + qemu + virt-manager
     ./tty-theme.nix # catppuccin-mocha
     # ./flatpak.nix
     ./steam.nix
-    ./wayvnc.nix
-    ./ollama.nix
     ./wshowkeys.nix
     ./discord.nix
     ./firefox.nix
     # ./nvf.nix
     # ./clash.nix
-    ./mihomo.nix
     ./direnv.nix
     ./obs.nix
-    ./nfs.nix
     ./ax-shell.nix
-    ./sunshine.nix
     ./nvim.nix
     ./quickshell.nix
     ./ags.nix
+
+    # server
+    ./mpd.nix
+    ./nfs.nix
     ./nbfc.nix
+    ./ollama.nix
+    ./mihomo.nix
+    ./rclone.nix
+    ./sunshine.nix
+    ./wayvnc.nix
+    ./docker.nix
+    ./jellyfin.nix
   ];
 }
---
+++ programs/jellyfin.nix
@@ -0,0 +1,23 @@
+{
+  username,
+  config,
+  pkgs,
+  ...
+}: {
+  environment.systemPackages = with pkgs; [
+    jellyfin
+    jellyfin-web
+    jellyfin-ffmpeg
+    jellyfin-tui
+    jellyfin-rpc
+    jellyfin-mpv-shim
+  ];
+
+  services.jellyfin = {
+    enable = true;
+    user = "${username}"; # 允许它看到这些外部驱动器挂载的最简单方法是更改服务的用户 sudo chown -R /var/lib/jellyfin
+    openFirewall = true;
+  };
+
+  users.users.${username}.extraGroups = ["jellyfin"]; # 将用户添加到 jellyfin 组
+}
