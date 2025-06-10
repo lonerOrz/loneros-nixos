359d02dc751d7e11fda85aac47d075f11ce1e67d
loner <2788892716@qq.com>
2025-06-10 22:26:42 +0800

    pinyin -> rime

--- programs/fcitx5.nix
+++ programs/fcitx5.nix
@@ -13,12 +13,17 @@
         waylandFrontend = true; #
         # plasma6Support = true; #不用kde6 貌似不用启用
         addons = with pkgs; [
-          fcitx5-chinese-addons
+          # Rime
+          fcitx5-rime
+          rime-data
+
           fcitx5-gtk # Fcitx5 gtk im module and glib based dbus client library
           kdePackages.fcitx5-qt
           fcitx5-material-color
-          fcitx5-pinyin-zhwiki # 中文维基百科词条
-          fcitx5-pinyin-moegirl # 萌娘百科词条
+
+          # fcitx5-chinese-addons
+          # fcitx5-pinyin-zhwiki # 中文维基百科词条
+          # fcitx5-pinyin-moegirl # 萌娘百科词条
         ];
         #ignoreUserConfig = true; #启用不光个人设置无效，个人词库也会无法保存
         settings = {
