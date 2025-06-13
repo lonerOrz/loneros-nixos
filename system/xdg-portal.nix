b75d6ade2dc54030f06856a24e09f4294ed145e8
loner <2788892716@qq.com>
2025-06-13 12:51:04 +0800

    add wlr remoteDesktop path and default portal settings

--- system/xdg-portal.nix
+++ system/xdg-portal.nix
@@ -5,13 +5,29 @@
     wlr.enable = true;
     xdgOpenUsePortal = true;
     extraPortals = [
-      pkgs.xdg-desktop-portal-gtk
       pkgs.xdg-desktop-portal
+      pkgs.xdg-desktop-portal-gtk
     ];
     configPackages = [
+      pkgs.xdg-desktop-portal
       pkgs.xdg-desktop-portal-gtk
       pkgs.xdg-desktop-portal-hyprland
-      pkgs.xdg-desktop-portal
+      pkgs.xdg-desktop-portal-wlr_git # includes RemoteDesktop patch
     ];
+    config = {
+      # 默认 fallback 配置
+      common = {
+        default = ["gtk"];
+      };
+      hyprland = {
+        default = ["hyprland" "gtk"];
+      };
+      niri = {
+        default = ["wlr" "gtk"];
+      };
+      sway = {
+        default = ["wlr" "gtk"];
+      };
+    };
   };
 }
