ff22fe3f001f1a3b85a34f7b5f00c66b408a6a9d
loner <2788892716@qq.com>
2025-06-17 07:36:41 +0800

    nix-community cachix

--- system/nix.nix
+++ system/nix.nix
@@ -30,10 +30,12 @@
       substituters = [
         "https://hyprland.cachix.org"
         "https://cache.garnix.io" # add garnix cache form github loneros-nixos repo
+        "https://nix-community.cachix.org"
       ];
       trusted-public-keys = [
         "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
         "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       ];
       trusted-users = [
         "root"
