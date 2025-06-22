f8ac13dba020a326759f130f0e679bf2a6f70e09
loner <2788892716@qq.com>
2025-06-22 18:21:01 +0800

    cacenl autologin

--- system/greet.nix
+++ system/greet.nix
@@ -13,12 +13,19 @@ in
     settings = {
       default_session = {
         user = "${username}";
-        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --cmd ${session} -g 'Welcome to the loneros!' --user-menu --time --time-format '%A, %B %d, %Y - %I:%M:%S %p' --asterisks --greet-align center --theme border=magenta;text=cyan;prompt=green;time=red;action=blue;button=yellow;container=black;input=red"; # start Hyprland with a TUI login manager
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet -g 'Welcome to the loneros!' \
+          --user-menu \
+          --time \
+          --time-format '%A, %B %d, %Y - %I:%M:%S %p' \
+          --asterisks \
+          --greet-align center \
+          --theme border=magenta;text=cyan;prompt=green;time=red;action=blue;button=yellow;container=black;input=red"; # start Hyprland with a TUI login manager
       };
-      initial_session = {
-        user = "${username}";
-        command = "${session}";
-      };
+      # 自动登录: --cmd ${session}
+      # initial_session = {
+      #   user = "${username}";
+      #   command = "${session}";
+      # };
     };
   };
 }
