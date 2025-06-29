df5dc33e88ccb2af191973895685b669d0f21335
loner <2788892716@qq.com>
2025-06-30 01:25:50 +0800

    sub loneros cachix

--- system/nix.nix
+++ system/nix.nix
@@ -31,11 +31,13 @@
         "https://hyprland.cachix.org"
         "https://cache.garnix.io" # add garnix cache form github loneros-nixos repo
         "https://nix-community.cachix.org"
+        "https://loneros.cachix.org"
       ];
       trusted-public-keys = [
         "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
         "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "loneros.cachix.org-1:dVCECfW25sOY3PBHGBUwmQYrhRRK2+p37fVtycnedDU="
       ];
       trusted-users = [
         "root"
