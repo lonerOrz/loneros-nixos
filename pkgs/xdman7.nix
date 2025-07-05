4e8c28c979b0a894d50c7929aa126abfe9abeeb0
loner <2788892716@qq.com>
2025-07-06 05:12:26 +0800

    rebuild xdman

--- pkgs/xdman7.nix
+++ pkgs/xdman7.nix
@@ -8,12 +8,12 @@
   python3,
   yt-dlp,
 }:
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "xdman";
   version = "7.2.11";
 
   src = fetchurl {
-    url = "https://github.com/subhra74/xdm/releases/download/${version}/xdman.jar";
+    url = "https://github.com/subhra74/xdm/releases/download/${finalAttrs.version}/xdman.jar";
     hash = "sha256-gRfyhvneHlf0VRZ22PCrPi6ZBER0S1lffMTLngH1HHw=";
   };
 
@@ -40,24 +40,25 @@ stdenv.mkDerivation rec {
     mkdir -p $out/{bin,share/java/xdman}
     cp $src $out/share/java/xdman/xdman.jar
 
+    path=${
+      lib.makeBinPath [
+        ffmpeg
+        yt-dlp
+      ]
+    }
+
     makeWrapper ${jdk}/bin/java $out/bin/xdman \
-      --prefix PATH : ${
-        lib.makeBinPath [
-          ffmpeg
-          yt-dlp
-        ]
-      } \
       --set JAVA_HOME ${jdk.home} \
+      --prefix PATH : "$path" \
       --add-flags "-jar $out/share/java/xdman/xdman.jar"
 
     runHook postInstall
   '';
 
-  meta = with lib; {
+  meta = {
     description = "Xtreme Download Manager: download manager with multiple browser integrations";
     homepage = "https://xtremedownloadmanager.com/";
-    license = licenses.gpl2;
-    platforms = [ "x86_64-linux" ];
-    maintainers = with maintainers; [ lonerOrz ];
+    license = lib.licenses.gpl2;
+    platforms = lib.platforms.linux;
   };
-}
+})
--- pkgs/xdman8.nix
+++ pkgs/xdman8.nix
@@ -5,21 +5,18 @@
   makeWrapper,
   gtk3,
   ffmpeg,
+  librsvg,
   rpmextract,
   openssl,
   gsettings-desktop-schemas,
-  adwaita-icon-theme,
-  hicolor-icon-theme,
   shared-mime-info,
-  librsvg,
-  libcanberra-gtk3,
 }:
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "xdman8";
   version = "8.0.29";
 
   src = fetchurl {
-    url = "https://github.com/subhra74/xdm/releases/download/${version}/xdman_gtk-${version}-1.fc36.x86_64.rpm";
+    url = "https://github.com/subhra74/xdm/releases/download/${finalAttrs.version}/xdman_gtk-${finalAttrs.version}-1.fc36.x86_64.rpm";
     hash = "sha256-27R+f70DzIKqRniIYAPVbh1SIuy0pSqD4OZGH63CfqM=";
   };
 
@@ -32,11 +29,9 @@ stdenv.mkDerivation rec {
     gtk3
     ffmpeg
     openssl
+    librsvg
     gsettings-desktop-schemas
-    adwaita-icon-theme
-    hicolor-icon-theme
     shared-mime-info
-    libcanberra-gtk3
   ];
 
   unpackPhase = ''
@@ -45,69 +40,58 @@ stdenv.mkDerivation rec {
 
   dontBuild = true;
   dontConfigure = true;
-  dontPatchELF = true;
   dontStrip = true;
 
   installPhase = ''
     runHook preInstall
 
-    # Create the directory structure
     mkdir -p $out/{bin,opt/xdman,share/applications,share/icons/hicolor/scalable/apps}
-
-    # Copy the main application files
     cp -r opt/xdman/* $out/opt/xdman/
 
-    # Install icon
-    install -Dm644 opt/xdman/xdm-logo.svg $out/share/icons/hicolor/scalable/apps/
-
-    # Create wrapper
-    makeWrapper $out/opt/xdman/xdm-app $out/bin/xdman8 \
+    makeWrapper \
+      $out/opt/xdman/xdm-app \
+      $out/bin/xdman8 \
       --prefix PATH : "${lib.makeBinPath [ ffmpeg ]}" \
       --set DOTNET_SYSTEM_GLOBALIZATION_INVARIANT 1 \
       --prefix XDG_DATA_DIRS : "${gsettings-desktop-schemas}/share/gsettings-schemas/${gsettings-desktop-schemas.name}:${gtk3}/share/gsettings-schemas/${gtk3.name}:${shared-mime-info}/share:$out/share:$XDG_DATA_DIRS" \
-      --set GTK_THEME "Adwaita" \
-      --set GTK_PATH "${gtk3}/lib/gtk-3.0:${libcanberra-gtk3}/lib/gtk-3.0" \
-      --set GTK3_MODULES "canberra-gtk-module" \
-      --set GDK_PIXBUF_MODULE_FILE "${librsvg}/lib/gdk-pixbuf-2.0/2.10.0/loaders.cache" \
-      --set GDK_PIXBUF_MODULEDIR "${librsvg}/lib/gdk-pixbuf-2.0/2.10.0/loaders" \
       --set LD_LIBRARY_PATH "${
         lib.makeLibraryPath [
           stdenv.cc.cc.lib
           gtk3
           openssl
-          libcanberra-gtk3
+          librsvg
         ]
-      }"
+      }" \
+      --set GTK_THEME "Adwaita" \
+      --set GTK3_MODULES "${gtk3}/lib/gtk-3.0" \
+      --set GDK_PIXBUF_MODULE_FILE ""
 
-    # Install desktop file
-    install -Dm644 usr/share/applications/xdm-app.desktop $out/share/applications/xdman8.desktop
+    install -Dm644 usr/share/applications/xdm-app.desktop \
+      $out/share/applications/xdman8.desktop
 
-    # Patch the desktop file
     substituteInPlace $out/share/applications/xdman8.desktop \
       --replace "Exec=xdman" "Exec=xdman8" \
       --replace "Icon=xdm-app" "Icon=xdm-logo"
 
-    # Make binary executable and link libraries
     chmod +x $out/opt/xdman/xdm-app
     patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" \
              --set-rpath "${
                lib.makeLibraryPath [
-                 stdenv.cc.cc
+                 stdenv.cc.cc.lib
                  gtk3
                  openssl
-                 libcanberra-gtk3
+                 librsvg
                ]
              }" \
-     $out/opt/xdman/xdm-app
+             $out/opt/xdman/xdm-app
 
     runHook postInstall
   '';
 
-  meta = with lib; {
+  meta = {
     description = "Powerful download accelerator and video downloader";
     homepage = "https://github.com/subhra74/xdm";
-    license = licenses.gpl3;
-    platforms = [ "x86_64-linux" ];
-    maintainers = with maintainers; [ lonerOrz ];
+    license = lib.licenses.gpl2;
+    platforms = lib.platforms.linux;
   };
-}
+})
