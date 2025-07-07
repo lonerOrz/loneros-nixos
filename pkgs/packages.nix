879ba438252e6e2c94ca14d4ceeb2ada8dac8a42
loner <2788892716@qq.com>
2025-07-08 01:57:50 +0800

    auto callpackage pkgs/xxx/package.nix

--- pkgs/packages.nix
+++ pkgs/packages.nix
@@ -1,15 +1,56 @@
-# For auto update packages
 {
   pkgs ? import <nixpkgs> { },
+  dir ? ./.,
+  pkgFileName ? "package.nix",
 }:
+
 let
   lib = pkgs.lib;
-  list = builtins.readDir ./.;
-  files = builtins.attrNames (lib.filterAttrs (_: v: v == "regular") list);
+  entries = builtins.readDir dir;
+
+  excludedNames = [ "default.nix" ];
+  names = lib.filter (name: !(lib.elem name excludedNames)) (builtins.attrNames entries);
+
+  importDir = lib.foldl' (
+    acc: name:
+    let
+      path = dir + "/${name}";
+      kind = entries.${name};
+    in
+    if kind == "regular" && lib.hasSuffix ".nix" name then
+      let
+        relativePath = dir + "/${name}";
+        drv = pkgs.callPackage relativePath { };
+      in
+      acc
+      // {
+        "${lib.removeSuffix ".nix" name}" = drv // {
+          passthru = (drv.passthru or { }) // {
+            updateFile = relativePath;
+          };
+        };
+      }
+    else if kind == "directory" then
+      let
+        subPath = dir + "/${name}/${pkgFileName}";
+      in
+      if builtins.pathExists subPath then
+        let
+          drv = pkgs.callPackage subPath { };
+        in
+        acc
+        // {
+          "${name}" = drv // {
+            passthru = (drv.passthru or { }) // {
+              updateFile = subPath;
+            };
+          };
+        }
+      else
+        acc
+    else
+      acc
+  ) { } names;
+
 in
-builtins.listToAttrs (
-  map (file: {
-    name = lib.replaceStrings [ ".nix" ] [ "" ] file;
-    value = pkgs.callPackage ./${file} { };
-  }) files
-)
+importDir
