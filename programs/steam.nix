ab6ae80484a8add9eceb334dcf6a7e59c7f7b6a5
loner <2788892716@qq.com>
2025-07-19 17:58:48 +0800

    steam extraPkgs

--- programs/steam.nix
+++ programs/steam.nix
@@ -12,6 +12,21 @@
   programs = {
     steam = {
       enable = true;
+      package = pkgs.steam.override {
+        extraPkgs =
+          pkgs: with pkgs; [
+            xorg.libXcursor
+            xorg.libXi
+            xorg.libXinerama
+            xorg.libXScrnSaver
+            libpng
+            libpulseaudio
+            libvorbis
+            stdenv.cc.cc.lib
+            libkrb5
+            keyutils
+          ];
+      };
       extraCompatPackages = [ pkgs.proton-ge-bin ];
       gamescopeSession.enable = true;
       remotePlay.openFirewall = true;
