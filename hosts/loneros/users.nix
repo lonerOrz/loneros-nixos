57fc973e36cf7a24c339b72cbe2148ecb7a47bda
loner <2788892716@qq.com>
2025-07-23 20:16:43 +0800

    jujutsu

--- hosts/loneros/users.nix
+++ hosts/loneros/users.nix
@@ -56,7 +56,8 @@ in
       tectonic-unwrapped # TeX/LaTeX 公式渲染
       nixfmt-rfc-style # 官方 nixfmt 风格
       nixd # Nix lsp
-      gh
+      gh # github cli
+      jujutsu # better than git
 
       go-musicfox
       lazygit
