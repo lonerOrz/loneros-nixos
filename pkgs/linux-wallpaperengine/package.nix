1db2012c5728e9996d66bb9b5538bf6aa2322705
GitHub Action <action@github.com>
2025-08-01 03:47:22 +0000

    Update Custom packages

--- pkgs/linux-wallpaperengine/package.nix
+++ pkgs/linux-wallpaperengine/package.nix
@@ -51,7 +51,7 @@ let
 in
 stdenv.mkDerivation (finalAttrs: {
   pname = "linux-wallpaperengine";
-  version = "0-unstable-2025-07-31";
+  version = "0-unstable-2025-08-01";
 
   src = fetchFromGitHub {
     owner = "Almamu";
