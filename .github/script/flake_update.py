4d486cf7587e52bb683b50df2d47df792f196d34
loner <2788892716@qq.com>
2025-08-09 05:40:39 +0800

    custom update flake lock

--- .github/script/flake_update.py
+++ .github/script/flake_update.py
@@ -6,7 +6,7 @@ import sys
 from pathlib import Path
 
 # 🛑 要排除更新的 inputs
-EXCLUDED_INPUTS = {"lix-module"}  # "lix-module","nixpkgs"
+EXCLUDED_INPUTS = {"lix", "lix-module"}  # "lix-module","nixpkgs"
 
 
 def main():
