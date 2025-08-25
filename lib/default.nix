f9267d292204dc7d9aec8256206570e87fa9ad01
loner <2788892716@qq.com>
2025-08-25 23:58:06 +0800

    lib: add autoImport

---
+++ lib/default.nix
@@ -0,0 +1,28 @@
+{ lib }:
+
+let
+  autoImport = import ./autoimport.nix;
+in
+{
+  toUpperCase = str: builtins.toUpper str;
+  prefixedAttrs = prefix: attrs: lib.mapAttrs (name: value: { "${prefix}-${name}" = value; }) attrs;
+  packageVersions =
+    pkg: versions:
+    let
+      attrs = map (v: {
+        name = "${pkg}-${v}";
+        value = {
+          version = v;
+        };
+      }) versions;
+    in
+    lib.listToAttrs attrs;
+
+  importModule' =
+    path: extra:
+    import path {
+      lib = lib;
+      extra = extra;
+    };
+  inherit autoImport;
+}
