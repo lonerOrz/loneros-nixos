4d0d97aa63eb856e605d6b74e0083643b5ad7fc5
loner <2788892716@qq.com>
2025-08-28 04:31:37 +0800

    feat: clear home steam cache

--- programs/steam.nix
+++ programs/steam.nix
@@ -9,24 +9,39 @@
     "vm.max_map_count" = 2147483642;
   };
 
+  environment.systemPackages = with pkgs; [ mangohud ];
+
   programs = {
+    gamemode.enable = true;
     steam = {
       enable = true;
       package = pkgs.steam.override {
         extraPkgs =
           pkgs: with pkgs; [
-            xorg.libXcursor
-            xorg.libXi
-            xorg.libXinerama
-            xorg.libXScrnSaver
-            libpng
-            libpulseaudio
-            libvorbis
-            stdenv.cc.cc.lib
-            libkrb5
-            keyutils
+            freetype
+            SDL2
+            dbus
+            glib
           ];
+
+        extraLibraries =
+          pkgs: with pkgs; [
+            alsa-lib
+          ];
+        extraProfile = ''
+          export PROTON_LOG=1
+          export STEAM_FRAME_FORCE_CLOSE=1
+        '';
+        # clear $HOME
+        extraBwrapArgs = [
+          "--bind $HOME/.config/steam $HOME"
+        ];
+        extraEnv = {
+          LD_PRELOAD_32 = "";
+        };
+        privateTmp = true;
       };
+      fontPackages = [ pkgs.wqy_zenhei ];
       extraCompatPackages = [ pkgs.proton-ge-bin ];
       gamescopeSession.enable = true;
       remotePlay.openFirewall = true;
