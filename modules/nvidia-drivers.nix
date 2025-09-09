c5b0dc3ad4882159eb69cd80d77010b101b77dce
loner <2788892716@qq.com>
2025-09-10 02:04:28 +0800

    fix for nvidia-settings was already released

--- modules/nvidia-drivers.nix
+++ modules/nvidia-drivers.nix
@@ -97,8 +97,7 @@ in
       # Enable the Nvidia settings menu,
       # accessible via `nvidia-settings`.
 
-      # NOTE: https://github.com/chaotic-cx/nyx/issues/1178
-      nvidiaSettings = false;
+      nvidiaSettings = true;
 
       # Optionally, you may need to select the appropriate driver version for your specific GPU.
       package = nvidiaPackage;
