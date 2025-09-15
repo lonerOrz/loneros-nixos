872b43a7bbfeec5edcedfb1588994ab590c94d5b
loner <2788892716@qq.com>
2025-09-15 18:00:07 +0800

    flatpak: sjtu mirror

--- servers/flatpak.nix
+++ servers/flatpak.nix
@@ -15,8 +15,8 @@
     enable = true;
     remotes = lib.mkOptionDefault [
       {
-        name = "flathub-beta";
-        location = "https://flathub.org/beta-repo/flathub-beta.flatpakrepo";
+        name = "flathub";
+        location = "https://mirror.sjtu.edu.cn/flathub";
       }
     ];
     update = {
@@ -27,14 +27,24 @@
     };
     uninstallUnmanaged = false;
     uninstallUnused = true;
+    restartOnFailure = {
+      enable = true;
+      restartDelay = "60s";
+      exponentialBackoff = {
+        enable = false;
+        steps = 10;
+        maxDelay = "1h";
+      };
+    };
     packages = [
-      {
-        appId = "com.tencent.wemeet";
-        origin = "flathub";
-      }
+      # {
+      #   appId = "com.tencent.wemeet";
+      #   origin = "flathub";
+      # }
       # { appId = "com.brave.Browser"; origin = "flathub"; }
       # "com.obsproject.Studio"
       # "im.riot.Riot"
+      "com.tencent.wemeet"
     ];
     overrides = {
       # "com.brave.Browser".Context.filesystems = [ "home" ]; # 允许 Brave 访问主目录
