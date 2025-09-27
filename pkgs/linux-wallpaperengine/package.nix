8e95bfa9addaa9288b339104fb5462893edc85ec
loner <2788892716@qq.com>
2025-09-28 04:20:51 +0800

    linux-wallpaperengine: 0-unstable-2025-09-27 -> 0-unstable-2025-09-28

--- pkgs/linux-wallpaperengine/package.nix
+++ pkgs/linux-wallpaperengine/package.nix
@@ -51,7 +51,7 @@ let
 in
 stdenv.mkDerivation (finalAttrs: {
   pname = "linux-wallpaperengine";
-  version = "0-unstable-2025-09-27";
+  version = "0-unstable-2025-09-28";
 
   src = fetchFromGitHub {
     owner = "Almamu";
