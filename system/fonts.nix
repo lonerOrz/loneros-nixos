1af20a7185591eca1cf66d7868d40568385f942b
loner <2788892716@qq.com>
2025-09-29 22:05:47 +0800

    init fonts

--- system/fonts.nix
+++ system/fonts.nix
@@ -3,27 +3,57 @@
   ...
 }:
 {
-  # FONTS
-  fonts.packages = with pkgs; [
-    noto-fonts
-    fira-code
-    noto-fonts-cjk-sans
-    jetbrains-mono
-    font-awesome
-    terminus_font
-    #(nerdfonts.override {fonts = ["JetBrainsMono"];}) # stable banch
-    nerd-fonts.jetbrains-mono # unstable
-    nerd-fonts.fira-code # unstable
-    lxgw-wenkai-screen # 屏幕显示优化版
-    lxgw-wenkai # wenkai mono
-    victor-mono
-    nerd-fonts.fantasque-sans-mono # unstable
-    nerd-fonts.iosevka # rofi
-    maple-mono.NF-CN # 开源等宽中文
-    # quickshell
-    material-symbols
-    roboto
-    inter-nerdfont
-    nerd-fonts.caskaydia-mono
-  ];
+  fonts = {
+    packages = with pkgs; [
+      # 通用字体（支持多语言）
+      noto-fonts
+      noto-fonts-cjk-sans
+      noto-fonts-emoji
+
+      # 编程 / 等宽字体
+      jetbrains-mono
+      fira-code
+      nerd-fonts.jetbrains-mono
+      nerd-fonts.fira-code
+      nerd-fonts.iosevka
+      nerd-fonts.caskaydia-mono
+
+      # 中文 / 混合字体
+      lxgw-wenkai-screen
+      lxgw-wenkai
+
+      # 其他好看的字体（选项）
+      roboto
+      inter-nerdfont
+      victor-mono
+      material-symbols
+      maple-mono.NF-CN
+      nerd-fonts.fantasque-sans-mono
+    ];
+
+    # 不让系统自动安装默认字体（这样就不会自动装 DejaVu）
+    enableDefaultPackages = false;
+
+    fontconfig = {
+      enable = true;
+      defaultFonts = {
+        serif = [
+          "Noto Serif"
+          "emoji"
+        ];
+        sansSerif = [
+          "Inter Nerd Font"
+          "Noto Sans"
+          "emoji"
+        ];
+        monospace = [
+          "JetBrainsMono Nerd Font"
+          "FiraCode Nerd Font"
+          "Iosevka Nerd Font"
+          "emoji"
+        ];
+        emoji = [ "Noto Color Emoji" ];
+      };
+    };
+  };
 }
