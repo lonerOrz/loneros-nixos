89aded049ddfa0bb5fe3a8d2548f967baf052acc
loner <2788892716@qq.com>
2025-09-30 11:53:54 +0800

    systemd environment

--- servers/vicinae.nix
+++ servers/vicinae.nix
@@ -23,6 +23,14 @@ in
       Restart = "always";
       RestartSec = 5;
       KillMode = "process";
+
+      Environment = ''
+        PATH=$HOME/.nix-profile/bin:/run/current-system/sw/bin:/usr/bin
+        DISPLAY=:0
+        XDG_RUNTIME_DIR=/run/user/1000
+        WAYLAND_DISPLAY=wayland-1
+        DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus
+      '';
     };
 
     wantedBy = [ "default.target" ];
