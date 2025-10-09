24ff9f221794e92c85f46b9a17793c954b384e6e
lonerorz <68736947+lonerOrz@users.noreply.github.com>
2025-10-09 11:05:45 +0800

    Chore: auto update custom packages - 2025-10-09T02:55:55Z (#265)
    * linux-wallpaperengine: 0-unstable-2025-10-08 -> 0-unstable-2025-10-09
    
    * mpv-handler: 0.3.16 -> 0.4.1
    
    * Update Custom packages
    
    ---------
    
    Co-authored-by: GitHub Action <action@github.com>

--- pkgs/linux-wallpaperengine/package.nix
+++ pkgs/linux-wallpaperengine/package.nix
@@ -51,7 +51,7 @@ let
 in
 stdenv.mkDerivation (finalAttrs: {
   pname = "linux-wallpaperengine";
-  version = "0-unstable-2025-10-08";
+  version = "0-unstable-2025-10-09";
 
   src = fetchFromGitHub {
     owner = "Almamu";
