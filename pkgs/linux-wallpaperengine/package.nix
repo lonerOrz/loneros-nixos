0d59b03b3bdc6cf1a097edcaba391cbd1971d6b7
GitHub Action <action@github.com>
2025-10-19 03:10:06 +0000

    linux-wallpaperengine: 0-unstable-2025-10-18 -> 0-unstable-2025-10-19

--- pkgs/linux-wallpaperengine/package.nix
+++ pkgs/linux-wallpaperengine/package.nix
@@ -51,7 +51,7 @@ let
 in
 stdenv.mkDerivation (finalAttrs: {
   pname = "linux-wallpaperengine";
-  version = "0-unstable-2025-10-18";
+  version = "0-unstable-2025-10-19";
 
   src = fetchFromGitHub {
     owner = "Almamu";
