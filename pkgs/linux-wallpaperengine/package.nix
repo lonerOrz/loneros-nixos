88c5dd1b255ec08a1fd3c60ce140a3564ed69813
lonerorz <68736947+lonerOrz@users.noreply.github.com>
2025-10-31 11:15:18 +0800

    Chore: auto update custom packages - 2025-10-31T03:07:33Z (#313)
    * linux-wallpaperengine: 0-unstable-2025-10-30 -> 0-unstable-2025-10-31
    
    * Update Custom packages
    
    ---------
    
    Co-authored-by: GitHub Action <action@github.com>

--- pkgs/linux-wallpaperengine/package.nix
+++ pkgs/linux-wallpaperengine/package.nix
@@ -51,7 +51,7 @@ let
 in
 stdenv.mkDerivation (finalAttrs: {
   pname = "linux-wallpaperengine";
-  version = "0-unstable-2025-10-30";
+  version = "0-unstable-2025-10-31";
 
   src = fetchFromGitHub {
     owner = "Almamu";
