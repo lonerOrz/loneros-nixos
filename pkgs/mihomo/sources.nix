b6816be786064a8cd04b7885a1a64d35bdf75739
GitHub Action <action@github.com>
2025-11-19 03:04:36 +0000

    mihomo: 1.8.8 -> 1.8.9

--- pkgs/mihomo/sources.nix
+++ pkgs/mihomo/sources.nix
@@ -1,10 +1,10 @@
 {
   aarch64-linux = {
-    url = "https://github.com/mihomo-party-org/clash-party/releases/download/v1.8.8/clash-party-catalina-1.8.8-arm64.pkg";
-    hash = "sha256-Ybi4HIIH87FnmBb3DYJx6bo50YBzefX3sTQULP4G3xw=";
+    url = "https://github.com/mihomo-party-org/clash-party/releases/download/v1.8.9/clash-party-catalina-1.8.9-arm64.pkg";
+    hash = "sha256-dMLZEGVa1xwj8XbucmxJPMz+2eXmvVL6sG3QB5khTJk=";
   };
   x86_64-linux = {
-    url = "https://github.com/mihomo-party-org/clash-party/releases/download/v1.8.8/clash-party-linux-1.8.8-amd64.deb";
-    hash = "sha256-CijLo3yLQsHMtejIvRk41kWdv6VoZxOeHKawwqfG6gc=";
+    url = "https://github.com/mihomo-party-org/clash-party/releases/download/v1.8.9/clash-party-linux-1.8.9-amd64.deb";
+    hash = "sha256-i1pWgmrEUBLJxCNvFRGcqv36bBjgXYtaJMZVIQtKAfI=";
   };
 }
