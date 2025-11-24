3647fc3d32002ee037720a11b0801edc42c53029
loner <2788892716@qq.com>
2025-11-24 11:32:48 +0800

    feat: enable nvidia-container-toolkit

--- servers/podman.nix
+++ servers/podman.nix
@@ -55,7 +55,7 @@
   users.users.${username}.extraGroups = [ "podman" ];
 
   # waite https://github.com/NixOS/nixpkgs/pull/463702
-  # hardware.nvidia-container-toolkit.enable = true; # 直通 NVIDIA GPU
+  hardware.nvidia-container-toolkit.enable = true; # 直通 NVIDIA GPU
 
   # 开启 Podman 自动更新定时器（用户级 systemd）
   systemd.user.timers.podman-auto-update = {
