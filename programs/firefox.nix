4e76c8164ee96067437ac08013af85c9d002aa76
loner <2788892716@qq.com>
2025-12-06 19:31:13 +0800

    update flake

--- programs/firefox.nix
+++ programs/firefox.nix
@@ -6,7 +6,7 @@
 {
   environment.systemPackages = with pkgs; [
     # firefox
-    # firefox_nightly # waite https://github.com/NixOS/nixpkgs/pull/466490
+    # firefox_nightly # done https://github.com/NixOS/nixpkgs/pull/466490
 
     # zen
     inputs.zen-browser.packages."${pkgs.stdenv.hostPlatform.system}".twilight # default beta twilight
