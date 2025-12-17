4424e797b549736e45ec7853feea99177a4c6357
loner <2788892716@qq.com>
2025-12-18 02:46:27 +0800

    fix: mpd settings

--- servers/mpd.nix
+++ servers/mpd.nix
@@ -21,27 +21,28 @@
     dbFile = "/home/${username}/.config/mpd/database";
 
     # 指定 mpd.conf 配置文件路径
-    settings = ''
-      log_file                "/home/${username}/.config/mpd/log"
-      pid_file                "/home/${username}/.config/mpd/mpd.pid"
-      state_file              "/home/${username}/.config/mpd/state"
-      log_level               "default"
-      restore_paused          "yes"
-      auto_update             "yes"
-      auto_update_depth       "4"
+    settings = {
+      log_file = "/home/${username}/.config/mpd/log";
+      pid_file = "/home/${username}/.config/mpd/mpd.pid";
+      state_file = "/home/${username}/.config/mpd/state";
+      log_level = "default";
+      restore_paused = true;
+      auto_update = true;
+      auto_update_depth = 4;
 
-      audio_output {
-        type  "pipewire"
-        name  "Pipewire Sound Server"
-      }
-
-      audio_output {
-        type	"fifo"
-        name	"my_fifo"
-        path	"/home/${username}/.config/mpd/mpd.fifo"
-        format	"44100:16:2"
-      }
-    '';
+      audio_output = [
+        {
+          type = "pipewire";
+          name = "Pipewire Sound Server";
+        }
+        {
+          type = "fifo";
+          name = "my_fifo";
+          path = "/home/${username}/.config/mpd/mpd.fifo";
+          format = "44100:16:2";
+        }
+      ];
+    };
   };
   environment.systemPackages = with pkgs; [
     mpd
