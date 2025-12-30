22035f84496edce8dda7de65601731e3d9f86a36
loner <2788892716@qq.com>
2025-12-30 12:19:45 +0800

    pkga: add folo

--- hosts/loneros/users.nix
+++ hosts/loneros/users.nix
@@ -109,6 +109,7 @@ in
       # (dbeaver-bin.override { override_xmx = "1024m"; }) # 数据库管理
       door-knocker # protal check
       feishu
+      folo
 
       # electron wrapper
       obsidian-wrapper
