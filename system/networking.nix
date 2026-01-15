fe13be3684e5bf05c52c5dd5f3814372335b8e4e
loner <2788892716@qq.com>
2026-01-16 03:17:11 +0800

    use services.resolved.settings

--- system/networking.nix
+++ system/networking.nix
@@ -35,26 +35,53 @@
   };
 
   # DNS 解析服务
+  # services.resolved = {
+  #   enable = true;
+  #   dnssec = "false"; # allow-downgrade
+  #   dnsovertls = "opportunistic";
+  #   fallbackDns = [
+  #     "1.1.1.1"
+  #     "8.8.8.8"
+  #     "114.114.114.114"
+  #     "218.6.200.139"
+  #     "61.139.2.69"
+  #     "2606:4700:4700::1111" # Cloudflare IPv6
+  #     "2001:4860:4860::8888" # Google IPv6
+  #     "240c::6666" # 114DNS IPv6
+  #     "2400:3200::1" # 阿里DNS IPv6
+  #     "192.168.100.1"
+  #     "192.168.2.1"
+  #   ];
+  #   extraConfig = ''
+  #     CacheTTLSec=3600
+  #     DNSStubListener=yes
+  #   '';
+  # };
+
   services.resolved = {
     enable = true;
-    dnssec = "false"; # allow-downgrade
-    dnsovertls = "opportunistic";
-    fallbackDns = [
-      "1.1.1.1"
-      "8.8.8.8"
-      "114.114.114.114"
-      "218.6.200.139"
-      "61.139.2.69"
-      "2606:4700:4700::1111" # Cloudflare IPv6
-      "2001:4860:4860::8888" # Google IPv6
-      "240c::6666" # 114DNS IPv6
-      "2400:3200::1" # 阿里DNS IPv6
-      "192.168.100.1"
-      "192.168.2.1"
-    ];
-    extraConfig = ''
-      CacheTTLSec=3600
-      DNSStubListener=yes
-    '';
+    settings = {
+      Resolve = {
+        DNSOverTLS = "opportunistic";
+        DNSSEC = "false"; # allow-downgrade
+        FallbackDNS = [
+          "1.1.1.1"
+          "8.8.8.8"
+          "114.114.114.114"
+          "218.6.200.139"
+          "61.139.2.69"
+          "2606:4700:4700::1111" # Cloudflare IPv6
+          "2001:4860:4860::8888" # Google IPv6
+          "240c::6666" # 114DNS IPv6
+          "2400:3200::1" # 阿里DNS IPv6
+          "192.168.100.1"
+          "192.168.2.1"
+        ];
+        extraConfig = ''
+          CacheTTLSec=3600
+          DNSStubListener=yes
+        '';
+      };
+    };
   };
 }
