dcc884421d30cbd97da1ede05e7526ff8307a3fd
loner <2788892716@qq.com>
2026-02-08 17:31:45 +0800

    cf: add k8s service ingress

--- servers/cloudflared.nix
+++ servers/cloudflared.nix
@@ -8,12 +8,16 @@
 let
   domain = "lonerorz.dpdns.org";
 
-  services = {
+  hostServices = {
     router = 2026; # caddy
-    uptime = 16937;
+    # uptime = 16937;
     reader = 4396;
-    apps = 80;
   };
+
+  k8sServices = [
+    "uptime"
+    "apps"
+  ];
 in
 {
   environment.systemPackages = with pkgs; [
@@ -25,12 +29,24 @@ in
     # certificateFile = config.sops.secrets."cloudflared/cert_pem".path; # cloudflared 登录凭证文件
     tunnels.laptop = {
       credentialsFile = config.sops.secrets."cloudflared/tunnel_json".path; # tunnel ID 凭证
-      ingress = lib.mapAttrs' (name: port: {
-        name = "${name}.${domain}";
-        value = {
-          service = "http://127.0.0.1:${toString port}";
-        };
-      }) services;
+      ingress =
+        # Podman / Host
+        (lib.mapAttrs' (name: port: {
+          name = "${name}.${domain}";
+          value = {
+            service = "http://127.0.0.1:${toString port}";
+          };
+        }) hostServices)
+        //
+          # Kubernetes
+          (lib.listToAttrs (
+            map (name: {
+              name = "${name}.${domain}";
+              value = {
+                service = "http://127.0.0.1:31247";
+              };
+            }) k8sServices
+          ));
       default = "http_status:404";
     };
   };
