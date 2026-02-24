b44d0eafdf31f21b745e990c94ba08b94f7763d7
loner <2788892716@qq.com>
2026-02-24 20:24:19 +0800

    overlay deno

---
+++ overlays/deno.nix
@@ -0,0 +1,13 @@
+# waiting-pr https://github.com/NixOS/nixpkgs/pull/491807
+final: prev: {
+  deno = prev.deno.overrideAttrs (old: {
+    cargoTestFlags = [
+      "--lib" # unit tests
+      "--test=integration_test"
+
+      # Test targets not included here:
+      # - node_compat: heavy network usage
+      # - specs: custom harness without --skip support
+    ];
+  });
+}
--- overlays/pygame-ce.nix
+++ overlays/pygame-ce.nix
@@ -1,4 +1,4 @@
-# waiting-pr https://github.com/NixOS/nixpkgs/pull/483604
+# done https://github.com/NixOS/nixpkgs/pull/483604
 self: super: {
   python3Packages = super.python3Packages // {
     pygame-ce = super.python3Packages.pygame-ce.overrideAttrs (oldAttrs: {
