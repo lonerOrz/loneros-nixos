bc26777313d323dbf351f042c36fed5254e7f51e
loner <2788892716@qq.com>
2026-03-07 10:16:34 +0800

    fix atuin test

--- overlays/atuin/default.nix
+++ overlays/atuin/default.nix
@@ -1,8 +1,14 @@
 # https://github.com/Mic92/dotfiles/blob/main/home-manager/pkgs/atuin/0001-make-atuin-on-zfs-fast-again.patch
 self: super: {
-  atuin = super.atuin.overrideAttrs (oldAttrs: rec {
+  atuin = super.atuin.overrideAttrs (oldAttrs: {
     patches = oldAttrs.patches or [ ] ++ [
       ./atuin-zfs-fast.patch
+    ];
+
+    cargoTestFlags = (oldAttrs.cargoTestFlags or [ ]) ++ [
+      "--"
+      "--skip"
+      "dumb_random_test"
     ];
   });
 }
