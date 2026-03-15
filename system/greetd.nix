867b61f53562937e24cf28b7be53597c139de6ae
loner <2788892716@qq.com>
2026-03-15 10:25:54 +0800

    autologin

--- system/greetd.nix
+++ system/greetd.nix
@@ -6,6 +6,8 @@
   ...
 }:
 let
+  autoLogin = false;
+
   hypr-command = "${lib.getExe' config.programs.hyprland.package "Hyprland"}";
   niri-command = "${lib.getExe' config.programs.niri.package "niri-session"}";
 in
@@ -17,10 +19,12 @@ in
         user = "${username}";
         command = "${pkgs.tuigreet}/bin/tuigreet -g 'Welcome to the loneros!' --user-menu --time --time-format '%A, %B %d, %Y - %I:%M:%S %p' --asterisks --greet-align center --theme border=magenta;text=cyan;prompt=green;time=red;action=blue;button=yellow;container=black;input=red"; # start Hyprland with a TUI login manager
       };
-      # 自动登录: --cmd ${session}
+    }
+    # 自动登录: --cmd ${session}
+    // lib.optionalAttrs autoLogin {
       initial_session = {
-        user = "${username}";
-        command = "${niri-command}";
+        user = username;
+        command = niri-command;
       };
     };
   };
