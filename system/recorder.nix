8a4d907e6b73e5674f987fe15d86f17d5030bc4f
loner <2788892716@qq.com>
2026-04-10 20:45:38 +0800

    fix(recorder): remove gpu-screen-recorder security wrapper

--- system/recorder.nix
+++ system/recorder.nix
@@ -11,12 +11,6 @@
   ];
 
   security.wrappers = {
-    gpu-screen-recorder = {
-      owner = "root";
-      group = "root";
-      capabilities = "cap_sys_admin+ep";
-      source = lib.getExe pkgs.gpu-screen-recorder;
-    };
     gsr-kms-server = {
       owner = "root";
       group = "root";
