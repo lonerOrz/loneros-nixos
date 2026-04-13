5c2745328feb0dad44569e0b19f65eb22c06863e
lonerorz <68736947+lonerOrz@users.noreply.github.com>
2026-04-13 15:40:32 +0800

    Update flake.lock - 2026-04-12T18-31-56Z (#502)
    * Update flake.lock - 2026-04-12T18-31-56Z
    
    🔄 Updating 27 inputs (excluding: lix, lix-module)
    
    ✨ Update details:
    - chaotic: VxIQ%3D → Qplo%3D
    - chaotic/home-manager: aM2Y%3D → bjGU%3D
    - chaotic/jovian: 5Tf8%3D → iNAk%3D
    - chaotic/nixpkgs: kQlw%3D → R76E%3D
    - chaotic/rust-overlay: 9kxA%3D → 7KfM%3D
    - firefox: EYkk%3D → 1T4s%3D
    - firefox/nixpkgs: XWsk%3D → w%3D
    - home-manager: aM2Y%3D → ylfM%3D
    - hyprland: phsA%3D → ile0%3D
    - nix-index-database: qYoI%3D → ehhk%3D
    - nixpkgs: wJSs%3D → Le90%3D
    - nixpkgs-master: d9i4%3D → QXck%3D
    - nixpkgs-stable: 5Ltc%3D → 23Ss%3D
    - nur: Xnxg%3D → cY%3D
    - nvf: zoFU%3D → %2BI%3D
    - sops-nix: Hd1Q%3D → gIcY%3D
    - sops-nix/nixpkgs: QpD0%3D → Le90%3D
    - stylix: vEoc%3D → GdKY%3D
    - zen-browser: I5C8%3D → Jm7U%3D
    
    * cursor: fixed failed hash
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- overlays/code-cursor-wrapper.nix
+++ overlays/code-cursor-wrapper.nix
@@ -1,11 +1,29 @@
-# 添加 electrcon 应用的环境变量和参数
 self: super:
 let
-  sftname = "code-cursor"; # 软件名称
-  cmdname = "cursor"; # 命令行名称
+  sftname = "code-cursor";
+  cmdname = "cursor";
+
+  version = "3.0.16";
+  hash = "sha256-dN8tFSppIpO/P0Thst5uaNzlmfWZDh0Y81Lx1BuSYt0=";
+
+  fixedCursor = super.${sftname}.override {
+    fetchurl =
+      args:
+      if args ? url && builtins.match "\.*/Cursor-${version}-x86_64\\\.AppImage" args.url != null then
+        super.fetchurl (
+          args
+          // {
+            inherit hash;
+          }
+        )
+      else
+        super.fetchurl args;
+  };
 in
 {
-  "${sftname}-wrapper" = super.${sftname}.overrideAttrs (oldAttrs: {
+  ${sftname} = fixedCursor;
+
+  "${sftname}-wrapper" = fixedCursor.overrideAttrs (oldAttrs: {
     pname = "${sftname}-wrapper";
 
     nativeBuildInputs = (oldAttrs.nativeBuildInputs or [ ]) ++ [ super.makeWrapper ];
@@ -14,9 +32,9 @@ in
       echo "Wrapping cursor binary..."
       if [ -f "$out/bin/${cmdname}" ]; then
         wrapProgram "$out/bin/${cmdname}" \
-        --set ELECTRON_OZONE_PLATFORM_HINT auto \
-        --set LIBGL_ALWAYS_INDIRECT 1 \
-        --add-flags "--enable-features=UseOzonePlatform --ozone-platform=x11 --enable-wayland-ime --disable-gpu"
+          --set ELECTRON_OZONE_PLATFORM_HINT auto \
+          --set LIBGL_ALWAYS_INDIRECT 1 \
+          --add-flags "--enable-features=UseOzonePlatform --ozone-platform=x11 --enable-wayland-ime --disable-gpu"
       else
         echo "Warning: $out/bin/${cmdname} not found!"
       fi
