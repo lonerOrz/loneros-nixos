43f55522e1c46cfff2e4dcbcfeff7caf1985a81d
loner <2788892716@qq.com>
2026-04-18 03:09:34 +0800

    feat: niri zoom patch

--- overlays/niri/default.nix
+++ overlays/niri/default.nix
@@ -1,8 +1,18 @@
 # https://github.com/YaLTeR/niri/pull/1791
 self: super: {
-  niri-unstable = super.niri-unstable.overrideAttrs (oldAttrs: rec {
+  niri = super.niri.overrideAttrs (oldAttrs: {
     patches = oldAttrs.patches or [ ] ++ [
       ./support-shm-shareing.patch
     ];
   });
+
+  niri-git = super.nur.repos.lonerOrz.niri-git.overrideAttrs (oldAttrs: {
+    patches = oldAttrs.patches or [ ] ++ [
+      (super.fetchpatch2 {
+        url = "https://patch-diff.githubusercontent.com/raw/niri-wm/niri/pull/3246.patch";
+        hash = "sha256-wdiMj0dRmmf0/LcetJUEXZVPBlonQwY5lIx87a3BG6I=";
+      })
+    ];
+  });
+
 }
