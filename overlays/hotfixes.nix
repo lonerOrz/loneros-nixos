ee5fa78c3ae9a5c0804d5753bc7a07b4c7d1c9f5
loner <2788892716@qq.com>
2026-06-09 10:38:49 +0800

    fix: Electron version 39.8.10 is EOL

--- overlays/hotfixes.nix
+++ overlays/hotfixes.nix
@@ -5,7 +5,12 @@ final: prev:
 let
   pkgsMaster = import inputs.nixpkgs-master {
     system = final.stdenv.hostPlatform.system;
-    config.allowUnfree = true;
+    config = {
+      allowUnfree = true;
+      permittedInsecurePackages = [
+        "electron-39.8.10"
+      ];
+    };
   };
 
   hotfixes = {
